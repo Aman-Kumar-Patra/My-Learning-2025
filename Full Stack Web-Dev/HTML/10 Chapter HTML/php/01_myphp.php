b8cabb1f17c0203fff761746948a87dda2df2c91
Aman-Kumar-Patra <185592897+Aman-Kumar-Patra@users.noreply.github.com>
2025-02-25 17:59:04 +0530

    added 25-02-2025 learning file

---
+++ Full Stack Web-Dev/HTML/10 Chapter HTML/php/01_myphp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP in <HTML></HTML></title>
+</head>
+<body>
+    <h1>PHP in HTML</h1>
+    <?php 
+    echo "Hello World";
+    echo 2+3
+    ?>
+</body>
+</html>
